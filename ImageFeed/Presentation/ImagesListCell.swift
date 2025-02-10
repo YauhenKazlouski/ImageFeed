7e547e28cf2ffb48529c3409cbcfbb06b1aa7237
YauhenKazlouski <yauhenkazlouskiswift@gmail.com>
2025-02-10 14:58:16 +0300

    Created custom cell

---
+++ ImageFeed/Presentation/ImagesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by PlAdmin on 9.02.25.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
