2aa6541e3114804ca83e884c01cdd763a660ea56
YauhenKazlouski <yauhenkazlouskiswift@gmail.com>
2025-02-12 18:24:21 +0300

    Added TabBar

---
+++ ImageFeed/Presentation/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by PandaPo on 12.02.25.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
