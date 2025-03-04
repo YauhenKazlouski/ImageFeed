511276588ec511e3c4911f5f4938a4a81d5594e0
YauhenKazlouski <yauhenkazlouskiswift@gmail.com>
2025-03-04 23:59:20 +0300

    Rechecked and corrected constants

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -9,7 +9,7 @@ import Foundation
 enum Constants{
     static let accessKey = "3WQdPgiZrCbS1yA1okine_luer-5hwHR1Wqe1nGPJZg"
     static let secretKey = "JV4lNtlCIfZxOtmqL4_a51R9y61Zr_Vin457Y5JCKUE"
-    static let redirectURI = "urn:ietf:wg:oauth:2.0:oo"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes"
     static var defaultBaseURL: URL {
         guard let url = URL(string: "https://api.unsplash.com") else {
